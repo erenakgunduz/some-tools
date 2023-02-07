350eba7f58d4293a694f066496eabdc373326213
erenakgunduz <coderenius@gmail.com>
2023-02-07 11:54:46 -0500

    Update with_that.py
    Code is more precise now :sunglasses:

--- with_that.py
+++ with_that.py
@@ -1,6 +1,15 @@
-you = "doomed"
+import re
 
-if type(you) is not bool:
-    print(f"No, you not {type(True)}")
-else:
-    raise SystemExit
+
+def main():
+    you = "doomed"
+    u_not = re.findall(r"'(.*?)'", f"{type(True)}")
+
+    if not isinstance(you, bool):
+        print(f"No, you not {u_not[0]}")
+    else:
+        raise SystemExit
+
+
+if __name__ == "__main__":
+    main()
